57e75f895393aefb398e7a19cdba65882f5a58c9
Joel Voss <mail@joelvoss.com>
2020-06-02 20:23:09 +0200

    misc(taskfile): update `Taskfile.sh`

--- Taskfile.sh
+++ Taskfile.sh
@@ -3,6 +3,13 @@ PATH=./node_modules/.bin:$PATH
 
 # //////////////////////////////////////////////////////////////////////////////
 
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+# //////////////////////////////////////////////////////////////////////////////
+
 # Default task (executed when no <task> argument was given)
 function default {
   validate
@@ -41,21 +48,14 @@ function clean {
 
 # //////////////////////////////////////////////////////////////////////////////
 
-function help {
-  printf "$(_now) Usage: $0 <task> <args>\n\n"
-}
-
 function _now {
   time=$(date +'%H:%M:%S')
-  printf "\e[2m[$time]\e[0m"
+  printf "\033[2m[$time]\033[0m"
 }
 
-function _checkmark {
-  printf "\e[1m\e[32m✓\e[0m"
-}
 
 TASK=${@:-default}
 
-printf "$(_now) \e[1m\e[96mTaskfile\e[0m $TASK\n\n"
+printf "$(_now) \033[1m\033[96mTaskfile\033[0m $TASK\n\n"
 ${@:-default}
 printf "\n"
